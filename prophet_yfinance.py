db107e1db4c49e9a08af574519577543b798e7bf
Elias Cid <ecp1594@gmail.com>
2024-08-25 19:30:17 -0300

    Adjusted numpy np.float_ to np.float64

--- prophet_yfinance.py
+++ prophet_yfinance.py
@@ -3,6 +3,8 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
+import numpy as np
+np.float_ = np.float64
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
